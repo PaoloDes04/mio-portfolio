a154c35fd015d2a2d531b53f3fb69d62a1f60e9d
Paolo Desiderio <paolo.desiderio001@studenti.uniparthenope.it>
2026-04-02 16:37:42 +0200

    Fix: added base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/mio-portfolio/', // <--- DEVE essere uguale al nome del repo su GitHub
+})
